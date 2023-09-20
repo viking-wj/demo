afaba02a0d2af599abcfee609548fced97781f5b
wj <wangjin@keenyoda.com>
2023-09-20 15:43:03 +0800

    feat: WebSocket demo

---
+++ notification/websocket/src/main/java/com/wj/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.wj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author wj
+ * @date 2023/9/20 15:21
+ */
+@Configuration
+
+public class WebSocketConfig  {
+
+    /**
+     * 	注入ServerEndpointExporter，
+     * 	这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
